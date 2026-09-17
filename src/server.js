c39bf259372e61d489fb26c013edaa28b7e089e1
Geyer Rodriguez Marin <geyer.rmrodriguez@gmail.com>
2026-09-17 18:25:29 -0500

    Creacion del controlador de compras

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import morgan from 'morgan';
 import routesUser from './routes/routesUser.js';
 import routerProduct from './routes/routesProduct.js';
+import routShopping from './routes/routesShopping.js';
 
 
 
@@ -10,6 +11,8 @@ server.use(morgan('dev'));
 server.use(express.json());
 server.use('/user', routesUser);
 server.use('/product',routerProduct);
+server.use('/shopping', routShopping);
+
 server.get('/',(req,res)=>{
     res.status(404).send("Not found");
 });
